90cd3de551ed42cf02b2e3b9830aa89e55fe1d33
wulianyou <31689208+wulianyou@users.noreply.github.com>
2019-11-27 18:07:35 +0800

    storage.js修改函数空格

--- src/common/js/storage.js
+++ src/common/js/storage.js
@@ -2,7 +2,7 @@ import storage from 'good-storage'
 
 const SELLER_KEY = '__seller__'
 
-export function saveToLocal(id, key, val) { // 存储逻辑
+export function saveToLocal (id, key, val) { // 存储逻辑
   const seller = storage.get(SELLER_KEY, {})
   if (!seller[id]) { // 区分不同商家
     seller[id] = {}
@@ -11,7 +11,7 @@ export function saveToLocal(id, key, val) { // 存储逻辑
   storage.set(SELLER_KEY, seller)
 }
 
-export function loadFromLocal(id, key, def) { // 读取逻辑
+export function loadFromLocal (id, key, def) { // 读取逻辑
   const seller = storage.get(SELLER_KEY, {})
   if (!seller[id]) {
     return def
